77d722a219fae8869ed2a8ade7161acf3b8e1966
MelvinLau98 <melvin98@hotmail.my>
2020-04-22 16:08:13 +0800

    l

---
+++ addallowance.php
@@ -0,0 +1,135 @@
+<?php
+    session_start();
+	include "conn.php";
+    if(empty($_SESSION["username"])){
+        header("location:index.php");
+    }
+    $username = $_SESSION["username"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Payroll Software - Add Employee</title>
+
+<!-- Bootstrap core CSS -->
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+<!-- Custom styles for this template -->
+<link href="css/simple-sidebar.css" rel="stylesheet">
+<link href="css/addemployee_button.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="d-flex" id="wrapper">
+
+<!-- Sidebar -->
+<?php include "sidebar.php"; ?>
+<!-- /#sidebar-wrapper -->
+
+<!-- Page Content -->
+<div id="page-content-wrapper">
+
+<?php include "navbar.php"; ?>
+    
+<div class="container-fluid">
+<h1 class="mt-4">Add Allowance Item</h1>
+<hr>
+<!-- dashboard conten here -->
+<section class="container py-4">
+    <div class="row">
+        <div class="col-md-12">
+            <br>
+
+			<div id="tabsJustifiedContent" class="tab-content">
+				
+                <div id="profile1" class="tab-pane fade active show">
+                    <div class="row pb-2">
+                        <div class="col-md-12">
+							<div class="container">
+								
+								<?php
+									if(isset($_POST["submit"])){
+										//get input data
+									$allowance_desc = $_POST["allowance_desc"];
+									$allowance_rate = $_POST["allowance_rate"];
+
+									$new_allowance_sql = "INSERT INTO allowance (allowance_desc, allowance_rate) VALUES (?,?)";
+
+									$prepared_stmt_insert = mysqli_prepare($conn, $new_allowance_sql);
+
+									mysqli_stmt_bind_param($prepared_stmt_insert, 'ss', $allowance_desc, $allowance_rate);
+
+
+									mysqli_stmt_execute($prepared_stmt_insert);
+									mysqli_stmt_close($prepared_stmt_insert);
+									}
+									?>
+								
+								<form class="form-horizontal" role="form" method="post">
+									
+									
+									<div class="form-group">
+										<label for="allowance_desc" class="col-sm-3 control-label"><h6>Item Description</h6></label>
+										<div class="col-sm-9">
+											<input type="text" id="allowance_desc" name="allowance_desc" placeholder="Description Example: Petrol" class="form-control" autofocus>
+										</div>
+									</div>
+									
+									<div class="form-group">
+										<label for="allowance_rate" class="col-sm-5 control-label"><h6>Rate of Item (RM)</h6></label>
+										<div class="col-sm-9">
+											<input type="number" id="allowance_rate" name="allowance_rate" step=".01" placeholder="Rate Example: 100" class="form-control" autofocus>
+										</div>
+									</div>
+									
+									<div class="col-sm-9">
+										<button type="submit" class="btn btn-primary btn-block" name="submit">Add Item</button>
+									</div>
+									
+								</form> <!-- /form -->
+							</div>
+						</div>
+					</div>
+                </div>
+				
+            </div>
+        </div>
+	</div>
+	
+</section>
+    
+    
+</div>
+</div>
+<!-- /#page-content-wrapper -->
+</div>	
+
+<!-- /#wrapper -->
+<!-- footer here -->
+<?php include "footer.php"; ?>   
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Menu Toggle Script -->
+<script>
+$("#menu-toggle").click(function(e) {
+e.preventDefault();
+$("#wrapper").toggleClass("toggled");
+});
+</script>
+
+
+</body>
+
+</html>
---
+++ addemployee_data.php
@@ -0,0 +1,53 @@
+<?php
+
+$host = "localhost";
+$dbusername = "root";
+$dbpass = "";
+$dbname = "payroll_db";
+									
+$conn = new mysqli($host, $dbusername, $dbpass, $dbname);
+
+$emp_full_name = $_POST["emp_full_name"];
+$emp_gender = $_POST["emp_gender"];
+$emp_dob = $_POST["emp_dob"];
+$emp_email = $_POST["emp_email"];
+$emp_address = $_POST["emp_address"];
+$emp_mobile = $_POST["emp_mobile"];
+$emp_telephone = $_POST["emp_telephone"];
+$emp_ic = $_POST["emp_ic"];
+$emp_passport = $_POST["emp_passport"];
+$emp_immigration = $_POST["emp_immigration"];
+$emp_title = $_POST["emp_title"];
+//---------------------------------------------------
+$emp_wages = $_POST["emp_wages"];
+$emp_payment_method = $_POST["emp_payment_method"];	
+$emp_bank_name = $_POST["emp_bank_name"];
+$emp_account = $_POST["emp_account"];
+$emp_health_status = $_POST["emp_health_status"];
+$emp_martial_status = $_POST["emp_martial_status"];
+$emp_spouse_status = $_POST["emp_spouse_status"];
+$emp_epf = $_POST["emp_epf"];
+$emp_socso = $_POST["emp_socso"];
+$emp_socso_type = $_POST["emp_socso_type"];
+$emp_eis_type = $_POST["emp_eis_type"];
+$emp_join_date = $_POST["emp_join_date"];
+$emp_confirm_date = $_POST["emp_confirm_date"];
+$emp_resign_date = $_POST["emp_resign_date"];
+$data_created_date = date("Y/m/d");
+
+
+$health_status=implode(", ", $emp_health_status);
+$spouse_status=implode(", ", $emp_spouse_status);
+
+										
+$new_employee_sql = "INSERT INTO employee_info (emp_full_name, emp_gender, emp_dob, emp_email, emp_address, emp_mobile, emp_telephone, emp_ic, emp_passport, emp_immigration, emp_title, emp_wages, emp_payment_method, emp_bank_name, emp_account, emp_health_status, emp_martial_status, emp_spouse_status, emp_epf, emp_socso, emp_socso_type, emp_eis_type, emp_join_date, emp_confirm_date, emp_resign_date, data_created_date) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+$prepared_stmt_insert = mysqli_prepare($conn, $new_employee_sql);
+
+mysqli_stmt_bind_param($prepared_stmt_insert, 'ssssssssssssssssssssssssss', $emp_full_name, $emp_gender, $emp_dob, $emp_email, $emp_address, $emp_mobile, $emp_telephone, $emp_ic, $emp_passport, $emp_immigration, $emp_title, $emp_wages, $emp_payment_method, $emp_bank_name, $emp_account, $health_status, $emp_martial_status, $spouse_status, $emp_epf, $emp_socso, $emp_socso_type, $emp_eis_type, $emp_join_date, $emp_confirm_date, $emp_resign_date, $data_created_date);
+
+mysqli_stmt_execute($prepared_stmt_insert);
+mysqli_stmt_close($prepared_stmt_insert);
+
+header("Location: addemployee.php"); 
+?>
