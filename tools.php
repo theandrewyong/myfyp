0fe1ede5411ed52558963081b1c7fa80997bae2d
andrew <theandrewyong@gmail.com>
2020-04-22 18:20:11 +0800

    updates

--- tools.php
+++ tools.php
@@ -42,49 +42,139 @@
 <div class="container-fluid">
 <h1 class="mt-4">Tools</h1>
 <hr>
-<!-- dashboard contenT here -->
     
-    <!-- EPF table -->
-    
-    <?php
-    //insert an id to process payroll
-    //get socso rate from process payroll table
-    
-            $soc_id = 13;
-        $select =  mysqli_query($conn, "SELECT * FROM socso_formula WHERE socso_formula_id = '$soc_id'");
+<!-- EPF table -->
+<h2>EPF Rate Table</h2>
+<div class="row">
+    <div class="col-12">
+        <div class="table-responsive">
+            <table id="example" class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th colspan="2">Wages</th>
+                        <th colspan="2">EPF</th>
+                    </tr>
+                    <tr>
+                        <td>Start With</td>
+                        <td>End With</td>
+                        <td>Employee EPF</td>
+                        <td>Employer EPF</td>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                $sql = "SELECT * FROM epf_formula";
+                $prepared_stmt_insert = mysqli_prepare($conn, $sql);
+                mysqli_stmt_execute($prepared_stmt_insert);
+                $result = $prepared_stmt_insert->get_result(); 
+                if($result->num_rows > 0) { 
+                    while ($data = $result->fetch_assoc()) {
+                        echo '<tr>';
+                        echo '<td>' . $data["epf_formula_wage_start"] . '</td>';
+                        echo '<td>' . $data["epf_formula_wage_end"] . '</td>';
+                        echo '<td>' . $data["epf_formula_employee_epf"] . '</td>';
+                        echo '<td>' . $data["epf_formula_employer_epf"] . '</td>';
+                        echo '</tr>';
+                    }
+                }
+                ?>            
+                </tbody>
+            </table>
+        </div>        
+    </div>
+</div>    
+<!-- EPF table end -->
         
-        while($data = mysqli_fetch_assoc($select)){
-            echo $data["socso_formula_id"];
-            $x = $data["socso_formula_id"];
-        }
-    echo $x;
-    if(isset($_POST["submit"])){
-
-        
-        //$socso_formula_id = 
-        
-        
-        $process_payroll_desc = mysqli_escape_string($conn, $_POST["pp_desc"]);
-        
-        $input_sql = "INSERT INTO process_payroll (socso_formula_id, process_payroll_desc) VALUES (?,?)";
-        $prepared_stmt_insert = mysqli_prepare($conn, $input_sql);
-        mysqli_stmt_bind_param($prepared_stmt_insert, 'ss', $x, $process_payroll_desc);
-        mysqli_stmt_execute($prepared_stmt_insert);
-        mysqli_stmt_close($prepared_stmt_insert);
-    }
+<!-- SOCSO table -->
+<h2>SOCSO Rate Table</h2>
+<div class="row">
+    <div class="col-12">
+        <div class="table-responsive">
+            <table id="example" class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th colspan="2">Wages</th>
+                        <th colspan="3">First Category</th>
+                        <th>Second Category</th>
+                    </tr>
+                    <tr>
+                        <td>Start With</td>
+                        <td>End With</td>
+                        <td>Employee Share</td>
+                        <td>Employer Share</td>
+                        <td>Total</td>
+                        <td>Employer Contribution</td>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                $sql = "SELECT * FROM socso_formula";
+                $prepared_stmt_insert = mysqli_prepare($conn, $sql);
+                mysqli_stmt_execute($prepared_stmt_insert);
+                $result = $prepared_stmt_insert->get_result(); 
+                if($result->num_rows > 0) { 
+                    while ($data = $result->fetch_assoc()) {
+                        echo '<tr>';
+                        echo '<td>' . $data["socso_formula_wage_start"] . '</td>';
+                        echo '<td>' . $data["socso_formula_wage_end"] . '</td>';
+                        echo '<td>' . $data["socso_formula_employee_amt"] . '</td>';
+                        echo '<td>' . $data["socso_formula_employer_amt"] . '</td>';
+                        echo '<td>' . $data["socso_total_amt"] . '</td>';
+                        echo '<td>' . $data["socso_employer_contribution_amt"] . '</td>';
+                        echo '</tr>';
+                    }
+                }
+                ?>            
+                </tbody>
+            </table>
+        </div>        
+    </div>
+</div>    
+<!-- SOCSO table end -->  
     
-    
-    
-    ?>
-    <form action="tools.php" method="post">
-    <input type="text" name="pp_desc">
-        <input type="submit" name="submit">
-    </form>
-    
-    
-    
-    
-    
+<!-- EIS table -->
+<h2>EIS Rate Table</h2>
+<div class="row">
+    <div class="col-12">
+        <div class="table-responsive">
+            <table id="example" class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th colspan="2">Wages</th>
+                        <th colspan="3">EIS</th>
+                    </tr>
+                    <tr>
+                        <td>Start With</td>
+                        <td>End With</td>
+                        <td>Employee EIS</td>
+                        <td>Employer EIS</td>
+                        <td>Total</td>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                $sql = "SELECT * FROM eis_formula";
+                $prepared_stmt_insert = mysqli_prepare($conn, $sql);
+                mysqli_stmt_execute($prepared_stmt_insert);
+                $result = $prepared_stmt_insert->get_result(); 
+                if($result->num_rows > 0) { 
+                    while ($data = $result->fetch_assoc()) {
+                        echo '<tr>';
+                        echo '<td>' . $data["eis_formula_wage_start"] . '</td>';
+                        echo '<td>' . $data["eis_formula_wage_end"] . '</td>';
+                        echo '<td>' . $data["eis_formula_employee_amt"] . '</td>';
+                        echo '<td>' . $data["eis_formula_employer_amt"] . '</td>';
+                        echo '<td>' . $data["eis_formula_total_amt"] . '</td>';
+                        echo '</tr>';
+                    }
+                }
+                ?>            
+                </tbody>
+            </table>
+        </div>        
+    </div>
+</div>    
+<!-- EIS table end -->    
 </div>
 </div>
 <!-- /#page-content-wrapper -->
