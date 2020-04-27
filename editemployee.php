e04cab76a40a7b95042171180d33c57f11215da8
MelvinLau98 <melvin98@hotmail.my>
2020-04-27 18:42:15 +0800

    lol

--- editemployee.php
+++ editemployee.php
@@ -43,7 +43,7 @@
 <h1 class="mt-4">Edit Employee</h1>
 <hr>
 <!-- dashboard conten here -->
-<section class="container py-4">
+<section class="container py-4 col-sm-12">
     <div class="row">
         <div class="col-md-12">
             <div class="p-5 bg-white rounded shadow mb-5">
@@ -61,7 +61,7 @@
                 </ul>
                 <div id="myTabContent" class="tab-content">
                     <div id="home" role="tabpanel" aria-labelledby="home-tab" class="tab-pane fade px-4 py-5 show active">
-							<div class="container">
+							<div class="container col-sm-12">
 								
 								<?php
 								  //submit_1
@@ -185,8 +185,8 @@
 								  ?>
 								
 								<form action="editemployee.php?emp_id=<?php echo "$get_emp_id"; ?>" method="POST">
+						<div class="row"><div class="col-sm-6"><!--left-------------------------------->
 									<a id="employee_section"><h1>Employee Main Profile</h1></a> 
-									<div class="col-sm-3"><a href="#payroll_section" class="btn btn-primary btn-block payroll_button">Payroll Details</a></div>
 										
 											<div class="col-md-9">
 												<label for="emp_id">Employee ID</label>
@@ -194,14 +194,14 @@
 											</div>
 										
 									  	<div class="form-group">
-										<label for="emp_full_name" class="col-sm-3 control-label"><h6>Full Name</h6></label>
+										<label for="emp_full_name" class="col-sm-3 control-label"><h5 class="pt-2">Full Name</h5></label>
 										<div class="col-sm-9">
 											<input type="text" id="emp_full_name" name="emp_full_name" placeholder="Full Name" class="form-control" value="<?php echo $show_emp_full_name; ?>" autofocus>
 										</div>
 										</div>
 									
 									<div class="form-group">
-										<label class="control-label col-sm-3"><h6>Gender<?php echo $show_emp_gender; ?></h6></label>
+										<label class="control-label col-sm-3"><h5 class="pt-2">Gender<?php echo $show_emp_gender; ?></h5></label>
 										<div class="col-sm-6">
 											<div class="row">
 												<div class="col-sm-4">
@@ -233,82 +233,97 @@
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_dob" class="col-sm-3 control-label"><h6>Date of Birth</h6></label>
+										<label for="emp_dob" class="col-sm-3 control-label"><h5 class="pt-2">Date of Birth</h5></label>
 										<div class="col-sm-9">
 											<input type="date" id="emp_dob" class="form-control" name="emp_dob" value="<?php echo $show_emp_dob; ?>">
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_email" class="col-sm-3 control-label"><h6>Email</h6></label>
+										<label for="emp_email" class="col-sm-3 control-label"><h5 class="pt-2">Email</h5></label>
 										<div class="col-sm-9">
 											<input type="email" id="emp_email" placeholder="Email" class="form-control" name= "emp_email" value="<?php echo $show_emp_email; ?>">
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_address" class="col-sm-3 control-label"><h6>Address</h6></label>
+										<label for="emp_address" class="col-sm-3 control-label"><h5 class="pt-2">Address</h5></label>
 										<div class="col-sm-9">
 											<input type="address" id="emp_address" placeholder="Address" class="form-control" name="emp_address" value="<?php echo $show_emp_address; ?>">
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_mobile" class="col-sm-3 control-label"><h6>Phone number</h6></label>
+										<label for="emp_mobile" class="col-sm-3 control-label"><h5 class="pt-2">Phone number</h5></label>
 										<div class="col-sm-9">
 											<input type="tel" id="emp_mobile" placeholder="Phone" class="form-control" name="emp_mobile" value="<?php echo $show_emp_mobile; ?>">
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_telephone" class="col-sm-5 control-label"><h6>Telephone number</h6></label>
+										<label for="emp_telephone" class="col-sm-5 control-label"><h5 class="pt-2">Telephone number</h5></label>
 										<div class="col-sm-9">
 											<input type="tel" id="emp_telephone" placeholder="Telephone" class="form-control" name="emp_telephone" value="<?php echo $show_emp_telephone; ?>">
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_ic" class="col-sm-3 control-label"><h6>IC</h6></label>
+										<label for="emp_ic" class="col-sm-3 control-label"><h5 class="pt-2">IC</h5></label>
 										<div class="col-sm-9">
 											<input type="ic" id="emp_ic" placeholder="IC/ID" class="form-control" name="emp_ic" value="<?php echo $show_emp_ic; ?>">
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_passport" class="col-sm-7 control-label"><h6>Passport Number (Optional)</h6></label>
+										<label for="emp_passport" class="col-sm-7 control-label"><h5 class="pt-2">Passport Number (Optional)</h5></label>
 										<div class="col-sm-9">
 											<input type="passport" id="emp_passport" placeholder="Passport Number" class="form-control" name="emp_passport" value="<?php echo $show_emp_passport; ?>">
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_immigration" class="col-sm-7 control-label"><h6>Immigration Number (Optional)</h6> </label>
+										<label for="emp_immigration" class="col-sm-7 control-label"><h5 class="pt-2">Immigration Number (Optional)</h5> </label>
 										<div class="col-sm-9">
 											<input type="immigration" id="emp_immigration" name="emp_immigration" placeholder="Immigration Number" class="form-control" value="<?php echo $show_emp_immigration; ?>">
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_title" class="col-sm-3 control-label"><h6>Job Title</h6></label>
+										<label for="emp_title" class="col-sm-3 control-label"><h5 class="pt-2">Job Title</h5></label>
 										<div class="col-sm-9">
 											<input type="text" id="emp_title" name="emp_title" placeholder="Job title description" class="form-control" value="<?php echo $show_emp_title; ?>">
 										</div>
 									</div>
-									
-									<br/>
-									<hr/>
-									<a id="payroll_section"><h1>Payroll Details</h1></a>
-									<div class="col-sm-5"><a href="#employee_section" class="btn btn-primary btn-block employee_button">Employee Main Profile</a></div>
+							
+									<div class="form-group">
+										<label for="emp_join_date" class="col-sm-3 control-label"><h5 class="pt-2">Employee Join Date</h5></label>
+										<div class="col-sm-9">
+											<input type="date" id="emp_join_date" name="emp_join_date" class="form-control" value="<?php echo $show_emp_join_date; ?>">
+										</div>
+									</div>
 									
 									<div class="form-group">
-										<label for="emp_wages" class="col-sm-3 control-label"><h6>Wages</h6></label>
+										<label for="emp_confirm_date" class="col-sm-3 control-label"><h5 class="pt-2">Employee Start Date</h5></label>
+										<div class="col-sm-9">
+											<input type="date" id="emp_confirm_date" name="emp_confirm_date" class="form-control" value="<?php echo $show_emp_confirm_date; ?>">
+										</div>
+									</div>
+							
+							</div><!--left--------------------------------------------------->		
+									<br/>
+									<hr/>
+					<div class="col-sm-6"><!--right--------------------------------------------------->
+									<a id="payroll_section"><h1>Payroll Details</h1></a>
+									
+									<div class="form-group">
+										<label for="emp_wages" class="col-sm-3 control-label"><h5 class="pt-2">Wages</h5></label>
 										<div class="col-sm-9">
 											<input type="number" id="emp_wages" name="emp_wages" placeholder="Employee Wages" class="form-control" value="<?php echo $show_emp_wages; ?>" autofocus>
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label class="control-label col-sm-3"><h6>Payment Method</h6></label>
+										<label class="control-label col-sm-3"><h5 class="pt-2">Payment Method</h5></label>
 										<div class="col-sm-6">
 											<div class="row">
 												<div class="col-sm-4">
@@ -351,7 +366,7 @@
 										</div>
 									</div>
 									<div class="form-group">
-										<label class="sel1 control-label col-sm-3"><h6>Bank Name</h6></label>
+										<label class="sel1 control-label col-sm-3"><h5 class="pt-2">Bank Name</h5></label>
 										<div class="col-sm-9">
 											<select class="form-control" id="emp_bank_name" name="emp_bank_name">
 												<option value="Maybank" <?php if($show_emp_bank_name == "Mabank"){echo 'selected="selected"';} else{} ?>>Maybank</option>
@@ -375,14 +390,14 @@
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_account" class="col-sm-3 control-label"><h6>Bank Account</h6></label>
+										<label for="emp_account" class="col-sm-3 control-label"><h5 class="pt-2">Bank Account</h5></label>
 										<div class="col-sm-9">
 											<input type="text" id="emp_account" placeholder="Bank Account Number" class="form-control" name= "emp_account" value="<?php echo $show_emp_account; ?>">
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label class="control-label col-sm-3"><h6>Health Status</h6></label>
+										<label class="control-label col-sm-3"><h5 class="pt-2">Health Status</h5></label>
 										<div class="col-sm-9">
 											<div class="row">
 												<div class="col-sm-3">
@@ -400,7 +415,7 @@
 									</div>
 									
 									<div class="form-group">
-										<label class="control-label col-sm-3"><h6>Martial Status</h6></label>
+										<label class="control-label col-sm-3"><h5 class="pt-2">Martial Status</h5></label>
 										<div class="col-sm-9">
 											<div class="row">
 												<div class="col-sm-3">
@@ -428,7 +443,7 @@
 									</div>
 									
 									<div class="form-group">
-										<label class="control-label col-sm-3"><h6>Spouse Status</h6></label>
+										<label class="control-label col-sm-3"><h5 class="pt-2">Spouse Status</h5></label>
 										<div class="col-sm-9">
 											<div class="row">
 												<div class="col-sm-3">
@@ -451,21 +466,21 @@
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_epf" class="col-sm-3 control-label"><h6>Employee EPF</h6></label>
+										<label for="emp_epf" class="col-sm-3 control-label"><h5 class="pt-2">Employee EPF</h5></label>
 										<div class="col-sm-9">
 											<input type="number" id="emp_epf" name="emp_epf" placeholder="Employee EPF Number" class="form-control" value="<?php echo $show_emp_epf; ?>" autofocus>
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_socso" class="col-sm-3 control-label"><h6>Employee SOCSO</h6></label>
+										<label for="emp_socso" class="col-sm-3 control-label"><h5 class="pt-2">Employee SOCSO</h5></label>
 										<div class="col-sm-9">
 											<input type="number" id="emp_socso" name="emp_socso" placeholder="Employee SOCSO Number" class="form-control" value="<?php echo $show_emp_socso; ?>" autofocus>
 										</div>
 									</div>
 									
 									<div class="form-group">
-										<label class="sel1 control-label col-sm-3"><h6>SOCSO Type</h6></label>
+										<label class="sel1 control-label col-sm-3"><h5 class="pt-2">SOCSO Type</h5></label>
 										<div class="col-sm-9">
 											<select class="form-control" id="emp_socso_type" name="emp_socso_type">
 												<option value="Category 1" <?php if($show_emp_socso_type == "Category 1"){echo 'selected="selected"';} else{} ?>>Category 1</option>
@@ -475,7 +490,7 @@
 									</div>
 									
 									<div class="form-group">
-										<label class="sel1 control-label col-sm-3"><h6>EIS Eligibility</h6></label>
+										<label class="sel1 control-label col-sm-3"><h5 class="pt-2">EIS Eligibility</h5></label>
 										<div class="col-sm-9">
 											<select class="form-control" id="emp_eis_type" name="emp_eis_type">
 												<option value="Yes" <?php if($show_emp_eis_type == "Yes"){echo 'selected="selected"';} else{} ?>>Yes</option>
@@ -485,33 +500,16 @@
 									</div>
 									
 									<div class="form-group">
-										<label for="emp_join_date" class="col-sm-3 control-label"><h6>Employee Join Date</h6></label>
-										<div class="col-sm-9">
-											<input type="date" id="emp_join_date" name="emp_join_date" class="form-control" value="<?php echo $show_emp_join_date; ?>">
-										</div>
-									</div>
-									
-									<div class="form-group">
-										<label for="emp_confirm_date" class="col-sm-3 control-label"><h6>Employee Start Date</h6></label>
-										<div class="col-sm-9">
-											<input type="date" id="emp_confirm_date" name="emp_confirm_date" class="form-control" value="<?php echo $show_emp_confirm_date; ?>">
-										</div>
-									</div>
-									
-									<div class="form-group">
-										<label for="emp_resign_date" class="col-sm-3 control-label"><h6>Employee Resign Date (Optional)</h6></label>
+										<label for="emp_resign_date" class="col-sm-3 control-label"><h5 class="pt-2">Employee Resign Date (Optional)</h5></label>
 										<div class="col-sm-9">
 											<input type="date" id="emp_resign_date" name="emp_resign_date" class="form-control" value="<?php echo $show_emp_resign_date; ?>">
 										</div>
 									</div>
 									
-										<div class="row">
-											<div class="col-md-6">
-												<br>
-												<input type="submit" class="btn btn-primary" name="submit_1" id="submit_1" value="Submit"> 
-											</div>
-										</div>
-									
+										<div class="form-group">
+										<div class="col-sm-9 pt-5"><button type="submit" class="btn btn-primary btn-block p-2">Register</button></div>
+									</div>
+							</div></div><!--right-------------------------------------------->		
 								  </form>
 							</div>
                     </div>
@@ -519,24 +517,24 @@
                     		<div class="container">
 								<h1>Assign Allowance</h1>
 								<form action="editemployee.php?emp_id=<?php echo "$get_emp_id"; ?>" method="POST">
-										<div class="col-sm-9">
+										<div class="col-sm-12">
 												<label for="emp_id">Employee ID</label>
 												<input type="text" class="form-control" name="emp_id" id="emp_id" value="<?php echo $get_emp_id; ?>" disabled>
 											</div>
 
 											<br/>
 											
-											<div class="col-sm-9">
+											<div class="col-sm-12">
 												<div class="form-group">
-												<label for="emp_full_name" class="control-label"><h6>Full Name</h6></label>
+												<label for="emp_full_name" class="control-label"><h5 class="pt-2">Full Name</h5></label>
 												<input type="text" id="emp_full_name" name="emp_full_name" placeholder="Full Name" class="form-control" value="<?php echo $show_emp_full_name; ?>" disabled>
 											</div>
 										</div>
 									
 										
 											<div class="form-group">
-												<div class="container"><label class="sel1 control-label"><h6>Allowance Type</h6></label></div>
-												<div class="col-sm-9">
+												<div class="container"><label class="sel1 control-label"><h5 class="pt-2">Allowance Type</h5></label></div>
+												<div class="col-sm-12">
 													<select class="form-control" id="select_allowance_desc" name="select_allowance_desc">
 														<?php
 															$select_all_sql = mysqli_query($conn, "SELECT * FROM allowance");
@@ -555,7 +553,7 @@
 										
 										<br/>
 									
-										<div class="col-sm-9">
+										<div class="col-sm-12">
 											<table id="example" class="table table-bordered">
 												<thead>
 													<tr>
@@ -570,7 +568,6 @@
 												<tbody>
 													
 											<?php
-											//$payroll_sql = "SELECT employee_allowance.emp_id, employee_info.emp_full_name, employee_allowance.allowance_id, employee_allowance.allowance_desc, employee_allowance.allowance_rate FROM employee_allowance INNER JOIN employee_info ON employee_info.emp_id = employee_allowance.emp_id INNER JOIN allowance ON allowance.allowance_id = employee_allowance.allowance_id";
 													
 											$table_sql = mysqli_query($conn, "SELECT * FROM employee_allowance WHERE emp_id = $get_emp_id");		
 											$allowance_total = 0;
@@ -601,27 +598,28 @@
 								</form>
 							</div>
                     </div>
+					
                     <div id="profilex" role="tabpanel" aria-labelledby="profilex-tab" class="tab-pane fade px-4 py-5">
                     		<div class="container">
 								<h1>Assign Dedcution</h1>
 								<form action="editemployee.php?emp_id=<?php echo "$get_emp_id"; ?>" method="POST">
-										<div class="col-sm-9">
+										<div class="col-sm-12">
 												<label for="emp_id">Employee ID</label>
 												<input type="text" class="form-control" name="emp_id" id="emp_id" value="<?php echo $get_emp_id; ?>" disabled>
 											</div>
 
 											<br/>
 											
-											<div class="col-sm-9">
+											<div class="col-sm-12">
 												<div class="form-group">
-												<label for="emp_full_name" class="control-label"><h6>Full Name</h6></label>
+												<label for="emp_full_name" class="control-label"><h5 class="pt-2">Full Name</h5></label>
 												<input type="text" id="emp_full_name" name="emp_full_name" placeholder="Full Name" class="form-control" value="<?php echo $show_emp_full_name; ?>" disabled>
 											</div>
 										</div>
 									
 										
-											<div class="form-group"><div class="container"><label class="sel1 control-label"><h6>Deduction Type</h6></label></div>
-												<div class="col-sm-9">
+											<div class="form-group"><div class="container"><label class="sel1 control-label"><h5 class="pt-2">Deduction Type</h5></label></div>
+												<div class="col-sm-12">
 													<select class="form-control" id="select_deduction_desc" name="select_deduction_desc">
 														<?php
 															$select_all_sql = mysqli_query($conn, "SELECT * FROM deduction");
@@ -640,7 +638,7 @@
 										
 										<br/>
 									
-										<div class="col-sm-9">
+										<div class="col-sm-12">
 											<table id="example" class="table table-bordered">
 												<thead>
 													<tr>
