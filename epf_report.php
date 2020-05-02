1e4357d436f322c66e7c4d5bba0784826152f1e5
andrew <theandrewyong@gmail.com>
2020-05-02 18:50:30 +0800

    updates

---
+++ epf_report.php
@@ -0,0 +1,64 @@
+<?php
+    session_start();
+    if(empty($_SESSION["username"])){
+        header("location:index.php");
+    }
+    $username = $_SESSION["username"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Payroll Software - Dashboard</title>
+
+<?php include "all_css.php"; ?>
+
+</head>
+
+<body>
+
+<div class="d-flex" id="wrapper">
+
+<!-- Sidebar -->
+<?php include "sidebar.php"; ?>
+<!-- /#sidebar-wrapper -->
+
+<!-- Page Content -->
+<div id="page-content-wrapper">
+
+<?php include "navbar.php"; ?>
+    
+<div class="container-fluid">
+<h1 class="mt-4">Dashboard</h1>
+<hr>
+<!-- dashboard conten here -->
+</div>
+</div>
+<!-- /#page-content-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+<!-- footer here -->
+<?php include "footer.php"; ?>    
+    
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Menu Toggle Script -->
+<script>
+$("#menu-toggle").click(function(e) {
+e.preventDefault();
+$("#wrapper").toggleClass("toggled");
+});
+</script>
+
+</body>
+
+</html>
